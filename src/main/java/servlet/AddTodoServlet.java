4efe4979f25868277152ea52378abc1720ff8daf
LyashenkoGS <lyashenkogs@gmail.com>
2015-09-16 20:54:36 -0700

    fix

---
+++ src/main/java/servlet/AddTodoServlet.java
@@ -0,0 +1,34 @@
+package servlet;
+
+import com.mvj.database.TODOdbWorker;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by LyashenkoGS on 9/16/2015.
+ */
+public class AddTodoServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        TODOdbWorker worker = new TODOdbWorker();
+
+        worker.addTODO((String) request.getParameter("TODO"), (String) request.getParameter("name"));
+        System.out.println((String) request.getParameter("TODO"));
+        System.out.println((String) request.getParameter("name"));
+        System.out.println("add todo servlet have been invoked");
+        Map todoMap = worker.getTODO();
+        System.out.println(todoMap);
+
+        request.setAttribute("todo", todoMap);
+
+        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/jsp/home.jsp");
+        rd.forward(request, response);
+    }
+
+}
---
+++ src/main/java/servlet/delTodo.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import com.mvj.database.TODOdbWorker;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Created by LyashenkoGS on 9/16/2015.
+ */
+public class delTodo extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        TODOdbWorker worker = new TODOdbWorker();
+
+        worker.deleteTODO(request.getParameter("TODO"));
+        System.out.println((String) request.getParameter("TODO"));
+        System.out.println("del todo servlet have been invoked");
+        Map todoMap = worker.getTODO();
+        System.out.println(todoMap);
+
+        request.setAttribute("todo", todoMap);
+
+        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/jsp/home.jsp");
+        rd.forward(request, response);
+    }
+
+}
